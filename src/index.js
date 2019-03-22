921ab51c2866540e218ae2695308cd8991364f5f
Bill Neff <BNeff@synacor.com>
2019-03-22 11:35:02 -0400

    Add getWrappedComponent() method on configured component Update all dependencies

--- src/index.js
+++ src/index.js
@@ -6,17 +6,19 @@ import delve from 'dlv';
  *	@param {Object} [defaults]		An object containing default configuration values
  *	@returns {Function} [configure()](#configure)
  *
- *	@example
+ *	@example <caption>Decorate a component without any namespace or defaults</caption>
  *	let configure = preconf();
- *	export default configure({ a: 'a' })(MyComponent);
+ *	export const MyConfiguredComponent = configure({ a: 'a' })(MyComponent);
  *
- *	@example
+ *	MyConfiguredComponent.getWrappedComponent() === MyComponent; // true
+ *
+ *	@example <caption>Decorate a component without a namespace and using defaults</caption>
  *	let configure = preconf(null, { url:'//foo.com' });
  *	export default configure({ url: 'url' })( props =>
  *		<a href={props.url} />
  *	);
  *
- *	@example
+ *	@example <caption>Decorate a component with weather namespace and using defaults</caption>
  *	let configure = preconf('weather', { url:'//foo.com' });
  *	export default configure({
  *		url: 'weather.url'
@@ -27,27 +29,31 @@ import delve from 'dlv';
 export default function preconf(namespace, defaults) {
 	if (namespace) defaults = { [namespace]: defaults };
 
-	/**	Creates a Higher Order Component that provides configuration as props.
+	/**	Creates a Higher Order Component (HOC) that provides configuration as props.
 	 *	@param {Object|Array<String>} keys	An object where the keys are prop names to pass down and values are dot-notated keypaths corresponding to values in configuration. If a string or array, prop names are inferred from configuration keys.
 	 *	@name configure
 	 *	@memberof preconf
-	 *	@returns {Function} configureComponent(Component) -> Component
+	 *	@returns {Function} configureComponent(Component) -> Component.  The resulting HOC has a method `getWrappedComponent()` that returns the Child that was wrapped
 	 */
 	return keys => {
 		if (typeof keys==='string') keys = keys.split(/\s*,\s*/);
 		let isArray = Array.isArray(keys);
 
-		return Child => (originalProps, context) => {
-			let props = {};
-			for (let i in originalProps) props[i] = originalProps[i];
-			for (let key in keys) {
-				let path = keys[key];
-				if (isArray) key = path.split('.').pop();
-				if (typeof props[key]==='undefined' || props[key]===null) {
-					props[key] = delve(context, 'config.'+path, delve(defaults, path));
+		return Child => {
+			function ConfigWrapper(originalProps, context) {
+				let props = {};
+				for (let i in originalProps) props[i] = originalProps[i];
+				for (let key in keys) {
+					let path = keys[key];
+					if (isArray) key = path.split('.').pop();
+					if (typeof props[key]==='undefined' || props[key]===null) {
+						props[key] = delve(context, 'config.'+path, delve(defaults, path));
+					}
 				}
+				return h(Child, props);
 			}
-			return h(Child, props);
+			ConfigWrapper.getWrappedComponent = Child && Child.getWrappedComponent || (() => Child);
+			return ConfigWrapper;
 		};
 	};
 }
